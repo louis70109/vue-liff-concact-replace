3e323d77620cf77872fe5044cceddc01a1a7f9bf
NiJia <louis70109@gmail.com>
2020-07-20 10:02:22 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>user id</h2>
+    <div>{{ userId }}</div>
+    <hr />
+    <h2>URL</h2>
+    <div>{{ msg }}</div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import liff from "@line/liff";
+
+export default {
+  name: "About",
+  data() {
+    return {
+      msg: "",
+      userId: "",
+    };
+  },
+  mounted() {
+    liff
+      .init({
+        liffId: "1654463380-xdPAJP8g",
+      })
+      .then((res) => {
+        if (!liff.isLoggedIn()) liff.login();
+
+        liff.getProfile().then((profile) => {
+          const userId = profile.userId;
+          this.userId = userId;
+          this.msg = window.location.href;
+        });
+      })
+      .catch((err) => {
+        alert(err.code, err.message);
+        alert("綁定異常，請聯絡作者");
+      });
+  },
+  methods: {},
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>user id</h2>
+    <div>{{ userId }}</div>
+    <hr />
+    <h2>URL</h2>
+    <div>{{ msg }}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import liff from "@line/liff";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      msg: "",
+      userId: "",
+    };
+  },
+  mounted() {
+    liff
+      .init({
+        liffId: "1654463380-RJGL0Gog",
+      })
+      .then((res) => {
+        if (!liff.isLoggedIn()) liff.login();
+
+        liff.getProfile().then((profile) => {
+          const userId = profile.userId;
+          this.userId = userId;
+          this.msg = window.location.href;
+        });
+      })
+      .catch((err) => {
+        alert(err.code, err.message);
+        alert("綁定異常，請聯絡作者");
+      });
+  },
+  methods: {},
+};
 </script>
